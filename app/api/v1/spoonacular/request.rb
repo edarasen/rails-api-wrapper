27b62153caa886bfa5ec603e1e6db395ca58727d
edarasen <edarasen@gmail.com>
2025-10-08 20:02:24 +0800

    error handling

--- app/api/v1/spoonacular/request.rb
+++ app/api/v1/spoonacular/request.rb
@@ -7,8 +7,6 @@ module V1::Spoonacular
       { "x-api-key" => ENV["SPOON_API_KEY"] }
     end
     def self.call(endpoint, options = {})
-      begin
-        response = {}
         headers = self.get_headers
         if !options.empty?
           response = get("#{endpoint}", headers: headers, query: options)
@@ -16,14 +14,11 @@ module V1::Spoonacular
           response = get("#{endpoint}", headers: headers)
         end
         if !response.success?
-          response = { error: response.code, data: Errors.map(response.code) }
+          raise HTTParty::Error.new(response.code)
         end
         response
       rescue HTTParty::Error => e
-        { error: e.message, status: :bad_request }
-      rescue StandardError => e
-        { error: e.message, status: :bad_gateway }
-      end
+        { error: e.message, status: Errors.map(e.message.to_i) }
     end
   end
 end
